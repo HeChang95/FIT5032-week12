ee6e37a6fbe2fdd622b0241716306b1992636648
HE CHANG <hcha0196@student.monash.edu>
2025-10-25 16:37:23 +1100

    build: fix Vite base to /FIT5032-week12/'

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/FIT5032-week12/',
+
   plugins: [
     vue(),
     vueDevTools(),
